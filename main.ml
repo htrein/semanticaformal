508256d9149d5e86180ea63eb7ef082c959a51f7
gabrielcarvalhoavila <gabrielmachado50@hotmail.com>
2025-06-16 22:05:54 -0300

    adding new things

--- main.ml
+++ main.ml
@@ -25,7 +25,7 @@ type expr =
     | Seq of expr * expr
     | Read
     | Print of expr
-    (*Não há let rec, app, fn*)    
+    | Loc of endereco  (* Adicionado: para representar endereços como valores *)
 
 
 (*Memória*)
@@ -44,6 +44,10 @@ let output : int list ref = ref []
 exception AlgumErro of string (*remover depois*)
 exception StepError of string
 exception TypeError of string
+exception NotValue of string
+exception ComputeFailed of string
+exception SubsError of string
+exception EvalError of string
 (*...*)
 (*Mensagens*) (*adicionar mais mensagens de exceções*)
 let msg_booleanos = "Ambos os operandos devem ser booleanos."
@@ -53,6 +57,10 @@ let msg_condicao = "A condição do if deve ser booleana."
 let msg_notvalue = "A expressão não é um valor."
 let msg_atribuicao =  "O primeiro argumento da atribuição deve ser uma referência do mesmo tipo do segundo."
 let msg_deref = "A expressão de desreferência deve ser uma referência."
+let msg_compute = "Erro na computação de operações aritméticas ou lógicas."
+let msg_subs = "Erro na substituição de variáveis."
+let msg_read = "Erro na leitura de entrada. A lista de entrada está vazia."
+let msg_stepError = "Erro ao dar um passo na execução da expressão."
 (*...*)
 
 (****************Avaliador***************************)
@@ -75,17 +83,27 @@ let compute (op: bop) (v1:expr) (v2:expr) : expr =
     | (Gt,Num n1, Num n2)  -> Bool (n1 > n2) 
     | (And, Bool b1, Bool b2) -> Bool (b1 && b2) 
     | (Or,  Bool b1, Bool b2) -> Bool (b1 || b2)                                  
-    |  _ -> raise AlgumErro
+    |  _ -> raise (ComputeFailed msg_compute)
 
 let rec subs (v:expr) (x:string) (e:expr) = 
     match e with 
     | Num _ -> e
     | Bool _ -> e
+    | Unit -> e
     | Binop(o,e1,e2) -> Binop(o,  subs v x e1,   subs v x e2)
     | If(e1,e2,e3) -> If(subs v x e1, subs v x e2, subs v x e3)           
     | Id y -> if x=y then v else e 
-    | Let(y,t,e1,e2) -> if x=y then Let(y,t,subs v x e1,e2) else Let(y,t,subs v x e1,subs v x e2)   
-    | _ -> raise AlgumErro
+    | Let(y,t,e1,e2) -> if x=y then Let(y,t,subs v x e1,e2) else Let(y,t,subs v x e1,subs v x e2)
+    | Wh(e1,e2) -> Wh(subs v x e1, subs v x e2)
+    | Asg(e1,e2) -> Asg(subs v x e1, subs v x e2)
+    | New(e1) -> New(subs v x e1)
+    | Deref(e1) -> Deref(subs v x e1)
+    | Seq(e1,e2) -> Seq(subs v x e1, subs v x e2)
+    | Read -> Read
+    | Print(e1) -> Print(subs v x e1)
+    | Loc l -> Loc l (* Adicionado: endereços não são substituídos *)
+    (*Tratamento de erro*)
+    | _ -> raise (SubsError msg_subs)
 
 let rec step (e:expr) (mem:memoria) (inp: int list) (out: int list): expr * memoria * int list * int list = 
     match e with
@@ -115,20 +133,26 @@ let rec step (e:expr) (mem:memoria) (inp: int list) (out: int list): expr * memo
     (*LET1*)
     | Let(x,t,e1,e2) -> let (e1',mem',inp',out') = step e1 mem in (Let(x,t,e1',e2),mem',inp',out') 
     (*E-WHILE*)
-    | Wh(e1,e2) -> step ((If(e1, Seq(e2, Wh(e1,e2)), Unit)), mem, inp, out) 
+    (*| Wh(e1,e2) -> step ((If(e1, Seq(e2, Wh(e1,e2)), Unit)), mem, inp, out)*)
+    | Wh(e1,e2) -> (If(e1, Seq(e2, Wh(e1,e2)), Unit), mem, inp, out)
     (*ATR1*) (*implementar aqui*)
-    | Asg(Num e1,e2) -> raise AlgumErro
+    | Asg(Loc l, v) when value v ->
+          let mem' = (l, v) :: List.remove_assoc l mem in
+          (Unit, mem', inp, out)
     (*ATR2*) (*verificar TyRef aqui*)
-    | Asg(TyRef l,e) -> let (e', mem', inp', out') = step e mem inp out in (Asg(TyRef l, e'), mem', inp', out') 
+    (*| Asg(TyRef l,e) -> let (e', mem', inp', out') = step e mem inp out in (Asg(TyRef l, e'), mem', inp', out') *)
+    | Asg(Loc l, e2) -> let (e2', m, i, o) = step e2 mem inp out in (Asg(Loc l, e2'), m, i, o)
     (*ATR*)
     | Asg(e1, e2) -> let (e1', mem', inp', out') = step e1 mem inp out in (Asg(e1', e2), mem', inp', out')
     (*NEW1*) (*implementar aqui*)
-    | New(v) when value v -> raise AlgumErro
+    | New v when value v ->
+          let l = List.length mem in
+          (Loc l, (l,v)::mem, inp, out)
     (*NEW*)
-    | New(e) -> let (e', mem', inp', out') = step e mem inp out in (New(e'), mem', inp', out')
+    | New e1 -> let (e1',m,i,o) = step e1 mem inp out in (New e1', m, i, o)
     (*DEREF1*)
-    | Deref(l) when value l -> 
-        (match procura l mem with
+    | Deref(Loc l) ->
+        (List.assoc_opt l mem with
         | Some v -> (v, mem, inp, out)
         | None -> raise (StepError msg_deref)
         )
@@ -138,26 +162,28 @@ let rec step (e:expr) (mem:memoria) (inp: int list) (out: int list): expr * memo
     | Seq(e1,e2) -> let (e1', mem', inp', out') = step e1 mem inp out in (Seq(e1',e2), mem', inp', out')
     (*SEQ1*) (*verificar TyUnit aqui*)
     | Seq(TyUnit e1, e2) -> (e2, mem, inp, out)
+    (*| Seq(Unit, e2) -> (e2, mem, inp, out) *) (*gepeto que fez isso, verificar se é necessário*)
+    (*| Seq(e1,e2) -> if typeinfer g e1 = TyUnit then typeinfer g e2 else raise (TypeError "seq deve começar com unit")*) (*gepeto que fez isso, verificar se é necessário*)
     (*READ*) (*verficar argumento do read*)
     | Read _    -> 
         (match inp with
         | n::rest -> (Num n, mem, rest, out)
-        | [] -> raise AlgumErro)
+        | [] -> raise (ReadError msg_read))
     (*PRINT-N*)
     | Print(Num e1) -> (Unit, mem, inp, !out @ [e1])
     (*PRINT*)
     | Print(e) -> let (e', mem', inp', out') = step e mem inp out in (Print(e'), mem', inp', out')
 
-    | _ -> raise AlgumErro  
+    | _ -> raise StepError msg_stepError
 
 
 
 let rec eval (e:expr) (mem:memoria): expr * memoria = 
     try 
-        let (e', mem') = step e mem in
+        let (e', mem', _, _) = step e mem in
         eval e' mem'
     with
-        AlgumErro -> (e, mem)
+        EvalError _ -> (e, mem)
 
 (*Funcao auxiliar para procurar um valor numa lista*)
 let rec procura valor lista = 
@@ -204,37 +230,37 @@ let rec typeinfer (g: tyEnv) (e: expr) : tipo =
     (*T-VAR*)           
     | Id x ->
         (match lookup g x with
-            None -> raise AlgumErro
+            None -> (TypeError "id não encontrado")
             | Some t -> t)
     (*T-LET*)             
     | Let(x,t,e1,e2) -> 
         let t1 = typeinfer g e1 in 
         let g' = (x,t)::g in
         let t2 = typeinfer g' e2 in
-        if t1=t then t2 else raise AlgumErro
+        if t1=t then t2 else raise (TypeError "tipo incompatível em let")
     (*T-WHILE*)
     | Wh(e1,e2) -> 
         let t1 = typeinfer g e1 in
         let t2 = typeinfer g e2 in
-        if(t1=TyBool) && (t2=TyUnit) then TyUnit else raise AlgumErro 
+        if(t1=TyBool) && (t2=TyUnit) then TyUnit else raise (TypeError "tipos incorretos no while")
     (*T-READ*)
     | Read -> TyInt
     (*T-SEQ*)
     | Seq(e1,e2) ->
         let t1 = typeinfer g e1 in
         let t2 = typeinfer g e2 in
-        if(t1=TyUnit) then t2 else raise AlgumErro
+        if(t1=TyUnit) then t2 else raise (TypeError "seq deve começar com unit")
     (*T-DEREF*)
     | Deref(e) ->
         let t1 = typeinfer g e in
         (match t1 with
         | TyRef(t) ->  t
-        | _ -> raise AlgumErro)
+        | _ -> raise (TypeError msg_deref)
     (*T-PRINT*)
     | Print(e) ->
         let t1 = typeinfer g e in 
         if(t1=TyInt) then TyUnit 
-        else raise AlgumErro
+        else raise (TypeError "print requer int")
     (*T-UNIT*)
     | Unit -> TyUnit
     (*T-NEW*)
@@ -249,7 +275,7 @@ let rec typeinfer (g: tyEnv) (e: expr) : tipo =
         | TyRef(t) when t = t2 -> TyUnit
         | TyRef _ -> raise (TypeError msg_atribuicao)
         | _ -> raise (TypeError msg_atribuicao))
-
+    | Loc _ -> raise (TypeError "loc não deve aparecer estaticamente")
 (***********************************************)
 
 (**************Interpretador********************)
@@ -259,6 +285,8 @@ let rec strofvalue (v:expr) : string = (*verificar*)
   match v with 
   | Num n -> string_of_int n
   | Bool b -> string_of_bool b
+  | Unit -> "()"
+  | Loc l -> "loc " ^ string_of_int l
   | _    -> raise (NotValue msg_notvalue)
     
 let rec stroftipo (t:tipo) : string = 
